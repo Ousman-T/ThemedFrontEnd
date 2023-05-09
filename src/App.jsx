5755bb41b400e9a332e1d0acc08a5c74af38c781
Ousman Touray <otouray14@gmail.com>
2023-05-09 08:51:19 -0400

    setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import About from './pages/About';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/home' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />}/>
+        </Routes>
+      </Router>
     </div>
   );
 }
