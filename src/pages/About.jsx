32b9980cee4983db9ec750369bc3af46a2cd101b
Ousman Touray <otouray14@gmail.com>
2023-05-09 08:45:54 -0400

    installing dependencies/setting up pages/routes

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About(){
+    return(
+        <div className="about">
+            <h1>About My Ambulance Company</h1>
+        </div>
+    )
+};
+
+export default About;
