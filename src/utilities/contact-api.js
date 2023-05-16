92b36b76df59d196776dfc88c139a68e77cf5b32
Ousman Touray <otouray14@gmail.com>
2023-05-16 07:22:40 -0400

    utils

--- src/utilities/contact-api.js
+++ src/utilities/contact-api.js
@@ -1,6 +1,26 @@
 const BASE_URL = '/contact'
 
+/*--- Helper Functions ---*/
+
+async function sendRequest(url, method = 'GET', payload = null) {
+    // Fetch accepts an options object as the 2nd argument
+    // used to include a data payload, set headers, etc.
+    const options = { method };
+    if (payload) {
+      options.headers = { 'Content-Type': 'application/json' };
+      options.body = JSON.stringify(payload);
+      console.log("here we are");
+    }
+}
+
+// * Create Contact
 export function createContact(){
     console.log('Creating contact through API');
+    return sendRequest(`${BASE_URL}`, 'POST', contactData);
     
-}
+};
+
+// ! Delete contact
+export function removeContact(){
+    return sendRequest(`${BASE_URL}`, 'DELETE', contactData);
+};
